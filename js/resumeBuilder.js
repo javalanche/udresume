6236c1e19a3f02ab5b4b47e976ef34c7355a1160
Javier Fraga <javier.s.fraga@gmail.com>
2014-11-27 20:21:14 -0500

    lesson1 update to file

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,26 @@
-$('#header').append('Javier Fraga');
-$('#main').append('Javier Fraga');
+//$('#header').append('Javier Fraga');
+//$('#main').append('Javier Fraga');
 //$("#main").append("Javier Fraga");
+
+var firstName = 'Javier';
+var age = 32;
+var awesomeThoughts = 'I am ' + firstName + ' and I am awesome';
+console.log(firstName);
+console.log(age);
+console.log(awesomeThoughts);
+
+var email = 'javier.s.fraga@att.com';
+var newEmail = email.replace('att', 'gmail');
+console.log(email);
+console.log(newEmail);
+
+var name = 'Javier Fraga';
+var role = 'Developer';
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+$('#header').prepend(formattedRole);
+$('#header').prepend(formattedName);
+
+var skills = ['awesomeness' , 'programming' , 'teaching' , 'JS'];
+//$('#main').append(skills);
+$('#main').append(skills[0]);
