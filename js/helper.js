d3ff3f981c8d16a7a2c38b6231269cb0406866fa
Javier Fraga <javier.s.fraga@gmail.com>
2014-12-06 16:57:46 -0500

    fixed href link taking up width of whole website by taking out the display:block in css for <a> tags and turned github in header to a link

--- js/helper.js
+++ js/helper.js
@@ -20,7 +20,7 @@ var HTMLcontactGeneric = "<li class='flex-item'><span class='orange-text'>%conta
 var HTMLmobile = "<li class='flex-item'><span class='orange-text'>mobile</span><span class='white-text'>%data%</span></li>";
 var HTMLemail = "<li class='flex-item'><span class='orange-text'>email</span><span class='white-text'>%data%</span></li>";
 var HTMLtwitter = "<li class='flex-item'><span class='orange-text'>twitter</span><span class='white-text'>%data%</span></li>";
-var HTMLgithub = "<li class='flex-item'><span class='orange-text'>github</span><span class='white-text'>%data%</span></li>";
+var HTMLgithub = "<li class='flex-item'><span class='orange-text'>github</span><a href='%url%' target='_blank' class='white-text'>%data%</a></li>";
 var HTMLblog = "<li class='flex-item'><span class='orange-text'>blog</span><span class='white-text'>%data%</span></li>";
 var HTMLlocation = "<li class='flex-item'><span class='orange-text'>location</span><span class='white-text'>%data%</span></li>";
 
@@ -31,30 +31,30 @@ var HTMLskillsStart = "<h3 id='skillsH3'>Skills at a Glance:</h3><ul id='skills'
 var HTMLskills = "<li class='flex-item'><span class='white-text'>%data%</span></li>";
 
 var HTMLworkStart = "<div class='work-entry'></div>";
-var HTMLworkEmployer = "<a href='%url%' target='_blank'>%data%";
-var HTMLworkTitle = " - %data%</a>";
+var HTMLworkEmployer = "<p><a href='%url%' target='_blank'>%data%";
+var HTMLworkTitle = " - %data%</a></p>";
 var HTMLworkDates = "<div class='date-text'>%data%</div>";
 var HTMLworkLocation = "<div class='location-text'>%data%</div>";
 var HTMLworkDescription = "<p><br>%data%</p>";
 
 var HTMLprojectStart = "<div class='project-entry'></div>";
-var HTMLprojectTitle = "<a href='%url%' target='_blank'>%data%</a>";
+var HTMLprojectTitle = "<p><a href='%url%' target='_blank'>%data%</a></p>";
 var HTMLprojectDates = "<div class='date-text'>%data%</div>";
 var HTMLprojectDescription = "<p><br>%data%</p>";
 var HTMLprojectImage = "<img src='%data%'>";
 
 var HTMLschoolStart = "<div class='education-entry'></div>"
-var HTMLschoolName = "<a href='%url%' target='_blank'>%data%";
-var HTMLschoolDegree = " -- %data%</a>";
+var HTMLschoolName = "<p><a href='%url%' target='_blank'>%data%";
+var HTMLschoolDegree = " -- %data%</a></p>";
 var HTMLschoolDates = "<div class='date-text'>%data%</div>";
 var HTMLschoolLocation = "<div class='location-text'>%data%</div>";
 var HTMLschoolMajor = "<em><br>Major: %data%</em>"
 
 var HTMLonlineClasses = "<h3>Online Classes</h3>";
-var HTMLonlineTitle = "<a href='%url%' target='_blank'>%data%";
-var HTMLonlineSchool = " - %data%</a>";
+var HTMLonlineTitle = "<p><a href='%url%' target='_blank'>%data%";
+var HTMLonlineSchool = " - %data%</a></p>";
 var HTMLonlineDates = "<div class='date-text'>%data%</div><br>";
-var HTMLonlineURL = "<br><a href='%url%' target='_blank'>%data%</a>";
+var HTMLonlineURL = "<br><p><a href='%url%' target='_blank'>%data%</a></p>";
 
 var internationalizeButton = "<button>Internationalize</button>";
 var googleMap = "<div id='map'></div>";
